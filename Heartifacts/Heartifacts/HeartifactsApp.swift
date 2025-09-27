2b738f323ac39134b1cda2e29f07c3a9867ba916
Shaya Farahmand <sfarahmand3@gatech.edu>
2025-09-27 10:16:58 -0400

    Initial template build

---
+++ Heartifacts/Heartifacts/HeartifactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeartifactsApp.swift
+//  Heartifacts
+//
+//  Created by Shaya Farahmand on 9/27/25.
+//
+
+import SwiftUI
+
+@main
+struct HeartifactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
