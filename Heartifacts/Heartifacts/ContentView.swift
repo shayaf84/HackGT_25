8e3ce9c6013ec5a1e60957ec5687f25fd7892b10
Shaya Farahmand <sfarahmand3@gatech.edu>
2025-09-27 10:16:58 -0400

    Initial template build

---
+++ Heartifacts/Heartifacts/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Heartifacts
+//
+//  Created by Shaya Farahmand on 9/27/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
